c8597d140af85e1765d88470c8c23b4098e4e554
Gerard <gerjoo@gmail.com>
2017-02-21 21:02:48 +0000

    adding setup.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='webpy-openshift-minimal-demo',
+      version='0.1',
+      description='Minimal working OpenShift web.py demo',
+      author='Gerard Meier',
+      author_email='gerjoo@gmail.com',
+      url='http://gerardmeier.com',
+      install_requires=['web.py'],
+)
